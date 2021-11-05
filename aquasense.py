735563704d5637ac72826e57dd497614dcedeb64
Alex <alexarroyo9912@gmail.com>
2021-11-05 15:23:33 -0500

    entry point

--- aquasense.py
+++ aquasense.py
@@ -4,10 +4,16 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', inicio="active")
 
+
 @app.route('/graficas')
 def graficas():
     return render_template('graficas.html', graficas="active")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
