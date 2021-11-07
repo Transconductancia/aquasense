9600b2af0f9ea71c1fa37d86e1d386cc3bf096d7
transconductancia <carlos.rodridelghams@gmail.com>
2021-11-07 12:03:15 -0500

    datos tiempo real desde ESP listos

--- aquasense.py
+++ aquasense.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flaskext.mysql import MySQL
 from decouple import config
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -17,11 +18,11 @@ mysql.init_app(app)
 
 def _datos(cur):
     cur.execute(
-        'SELECT fecha_adquisicion, numero1, numero2 FROM datos_tiempo_real WHERE id = (SELECT MAX(id) FROM datos_tiempo_real)')
+        'SELECT fecha_hora, agua_flujo FROM datos_dia WHERE id = (SELECT MAX(id) FROM datos_dia)')
     datos_tiempo_real = cur.fetchall()
-
+    print(datos_tiempo_real[0][0].strftime("%d/%m/%Y %H:%M:%S"))
     json_data = json.dumps(
-        {'fecha': datos_tiempo_real[0][0], 'numero1': datos_tiempo_real[0][1], 'numero2': datos_tiempo_real[0][2]})
+        {'fecha': datos_tiempo_real[0][0].strftime("%d/%m/%Y %H:%M:%S"), 'numero1': datos_tiempo_real[0][1]})
 
     yield f"data:{json_data}\n\n"
 
